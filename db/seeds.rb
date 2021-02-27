8cced1bd6594922ab4b33cc76018a08b32ff1980
Jessica Topley <jesse.topley@hotmail.com>
2021-02-27 17:08:47 +0000

    started to seed the activities with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'net/http'
+
+Activity.destroy_all
+
+event_url = 'https://api.list.co.uk/v1/events?location=London'
+uri = URI(event_url)
+req = Net::HTTP::Get.new(uri)
+req["Authorization"] = "Bearer "
+event_serialized = Net::HTTP.start(uri.hostname,uri.port,use_ssl: uri.scheme == "https") {|http| http.request(req)}
+# p event_serialized.body
+events = JSON.parse(event_serialized.body)
+
+# puts event.length
+
+p events.first
+
+events.each do |event|
+  activity = Activity.new
+  activity.name = event['name'].strip
+  activity.description = event['descriptions'][0]['description'].strip
+  puts activity.valid?
+  activity.save!
+end
+
+
