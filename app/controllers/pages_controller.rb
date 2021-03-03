b38a21179fef1b0069e16c4460e2ae7f0584f22e
Jessica Topley <jesse.topley@hotmail.com>
2021-03-03 01:06:55 +0000

    adding the pages controller and the favourites controller and the seach function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,18 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def index
+   
+    @restaurant = Restaurant.where(town: params[:query]).sample
+
+    @bar = Bar.where(town: params[:query]).sample
+
+    @activity = Activity.where(town: params[:query]).sample
+ 
+  end
+
+  def profile
+    @user = current_user
+  end
 end
