f485c0c3a589058355f76e3f89364e85663563d0
Miranda Frudd <mirandafrudd@outlook.com>
2021-03-13 11:43:08 +0000

    Added favourite show route controller and view

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,8 +1,10 @@
 class FavouritesController < ApplicationController
-
+  before_action :set_favourite, only: [:show]
   def index
     @favourites = Favourite.all
   end
+
+  def show; end
 
   def new
     @favourite = Favourite.new
@@ -12,10 +14,11 @@ class FavouritesController < ApplicationController
     @favourite = Favourite.new(favourite_params)
     if @favourite.save!
       redirect_to favourites_path
-    else 
+    else
       render :new
     end
   end
+
 
   def destroy
     @favourite = Favourite.find(params[:id])
@@ -23,6 +26,11 @@ class FavouritesController < ApplicationController
     redirect_to favourites_path
   end
 
+  private
+
+  def set_favourite
+    @favourite = Favourite.find(params[:id])
+  end
   def favourite_params
     params.require(:favourite).permit(:restaurant_id, :bar_id, :activity_id, :user_id)
   end
